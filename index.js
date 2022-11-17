ab9241b511d1fbacb8d70795f0aa9dd3ded7e1d1
Arthur <arthur.albert990@gmail.com>
2022-11-17 19:48:25 -0300

    Finish functions, add commentaries.

--- index.js
+++ index.js
@@ -1,23 +1,60 @@
 console.log('JS working.');
 
-//needs to be recursive
-// each "split" is a step
+// instructions
+// Example, to use call: mergeSort([1,6,0,45,20,16])
 
-//get input array
-//split into two sub arrays, "a" and "b"
-//sub array "a" and sub array "b"
-//split until you have only 1 number per array
-//sort it.
-//merge "a" and "b"
+//mergeSort:
+// keep splitting one array into two sub arrays until
+// the array has only one element, pass the two to mergeFunc
+//mergeFunc:
+// scans the arrays and keeps an index number for each
+// while index is less than array length, is has numbers to sort.
+// while it is sorted, the number is pushed in order to the finalArray
+// when index is = to array length, it has sorted all the numbers
+// return a full array with the sorted numbers
+// mergeSort
 
-function mergeSort(arr) {
-    if(arr.length % 2 == 0){
-        console.log('even')
-        let subArrA = arr.slice(0,arr.length/2)
-        let subArrB = arr.slice(arr.length/2,arr.length)
-        console.log(subArrA);
-        console.log(subArrB);
-    } else{
-        console.log('odd');
+
+function mergeFunc(subArrA,subArrB){
+    let finalArray =[];
+    let indexA = 0;
+    let indexB = 0;
+
+    //while conditions checks if "scanned" the full array
+    //keep track using an index
+    while (indexA < subArrA.length && indexB < subArrB.length){
+        let aNumber = subArrA[indexA];
+        let bNumber = subArrB[indexB];
+
+        //sort the numbers and push in right order
+        if (aNumber < bNumber){
+            finalArray.push(aNumber);
+            indexA++;
+        }
+        else{
+            finalArray.push(bNumber);
+            indexB++;
+        }
+
+        // console.log(finalArray);
     }
+
+    //returns an array with a copy of the arrays
+    return [...finalArray,...subArrA.slice(indexA),...subArrB.slice(indexB)]
+}
+
+function mergeSort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+
+    // slices into 2 arrays
+    middleIndexItem = Math.floor(arr.length/2)
+    let subArrA = arr.slice(0,middleIndexItem);
+    let subArrB = arr.slice(middleIndexItem,arr.length);
+
+    console.log(`A= ${subArrA}    B= ${subArrB}`);
+
+    //sort the arrays
+    return mergeFunc(mergeSort(subArrA),mergeSort(subArrB));
 }
